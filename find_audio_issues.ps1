63f7718ec7bbe470ac9bff855cac6c01d874f5d9
Victor Usoltsev <bernarden@mail.ru>
2019-06-28 23:27:00 +1200

    Fixes spelling.

--- find_audio_issues.ps1
+++ find_audio_issues.ps1
@@ -1,29 +1,29 @@
 # Variables
 $locationToSearch = "./";
-$problomaticAudioFormats = @("TrueHD", "E-AC-3")
+$problematicAudioFormats = @("TrueHD", "E-AC-3")
 
 # Logic
-# TODO: Include a file with previously scaned and conveted files to make this script faster.
+# TODO: Include a file with previously scanned and converted files to make this script faster.
 $allFiles = Get-ChildItem $locationToSearch -Include "*.*" -Recurse;
 ForEach ($file in $allFiles) {
     Write-Host "Checking file: $file"
     $audioFormatsString = ((mediainfo $file --Output='Audio;%Format%\n') | Out-String).Trim();
     $audioFormats = $audioFormatsString.Split('\r?\n');
     Write-Host "Found audio formats: '$($audioFormats -join ",")'"
-    $audioIssues = $audioFormats | Where-Object { $problomaticAudioFormats -contains $_ };
+    $audioIssues = $audioFormats | Where-Object { $problematicAudioFormats -contains $_ };
     if ($audioIssues.Length -eq 0) {
         Write-Host "No audio or audio issues found. Skipping file: '$file'"
         Write-Host "-------------------------"
         continue;
     }
 
-    # Note: Everything going foward has some audio issues.
+    # Note: Everything going forward has some audio issues.
     if ($audioFormats.Length -eq 1) {
-        Write-Host "Trying to automaticaly fix: '$file'"
+        Write-Host "Trying to automatically fix: '$file'"
         $newFileName = Join-Path $file.DirectoryName "$($file.BaseName)-1$($file.Extension)"
         $transcodeAudioOutput = ffmpeg -y -i "$file" -map 0 -c:v copy -c:a aac -c:s copy "$newFileName"
         
-        if ($LASTEXITCODE) {
+        if ($LastExitCode) {
             Write-Host "Failed to automatically resolve the issue with file: '$file'" 
             Remove-Item -Path $newFileName -Force
         }
